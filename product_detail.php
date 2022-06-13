d3c9a45fc8ea560258416ca8bff3a25e8a9a7f38
biancaa <biancafigueired.rodrigues@gmail.com>
2022-06-13 09:29:41 -0300

    aa

---
+++ product_detail.php
@@ -0,0 +1,26 @@
+<?php
+// includes basicos
+include_once './includes/conexao.php';
+include_once './includes/head.php';
+include_once './includes/header.php';
+?>
+
+<body>
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
