31250002b53ef35a072bf0578ef7935bf7bbf2c2
BiancaFiRodrigues <biancafigueired.rodrigues@gmail.com>
2022-06-20 08:43:37 -0300

    pagina sobre o produto

--- product_detail.php
+++ product_detail.php
@@ -6,8 +6,60 @@ include_once './includes/header.php';
 ?>
 
 <body>
+    <div class="container2">
+        <div class="product">
+            <div class="img-product">
 
+                <img src="images/Prod/<?php echo "p_alm_1.png";?>" alt="">
+
+            </div>
+
+            <div class="detail-prod">
+                <div class="detail">
+                    <h3><?php echo "Nome do produto"?></h3>
+                    <?php echo "Mix de Frutas"?>
+                </div>
+
+                <div class="btn_container">
+                    <a href="">Buy Now</a>
+                    
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <style>
+        .container2 {
+        width: 100vw;
+        height: 70vh;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        }
+
+        .img-product{
+            display: flex;
+            flex-direction: row;
+        }
+ 
+
+    </style>
+
+
+    <?php
     
+    
+    
+    
+    
+    
+    
+    
+    
+    ?>
 
 
 
