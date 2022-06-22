88750f3fa0bb1958c9685fe7f4aca457e8e78781
BiancaFiRodrigues <biancafigueired.rodrigues@gmail.com>
2022-06-22 15:00:53 -0300

    aa

--- includes/conexao.php
+++ includes/conexao.php
@@ -12,12 +12,5 @@ if (!$conn) {
 }
 
 
-$pesquisar = $_POST['pesquisar'];
-$result_produtos = "SELECT * FROM produtos WHERE nome LIKE '%$pesquisar%' LIMIT 5";
-$resultado_produtos = mysqli_query($conn, $result_produtos);
-
-while($rows_produtos = mysqli_fetch_array($resultado_produtos)){
-    echo "Nome dos produtos: ".$rows_produtos['nome']."<br>";
-}
 
 ?>
